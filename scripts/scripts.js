dae2a3d52e4bbccb88481dd2f63dfa77b2ae4a82
Nathan <antnefedov33@gmail.com>
2023-11-16 17:58:56 +0300

    wrote scripts and changed some styles

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,23 @@
 "use strict"
+
+let images = document.querySelector('.images');
+
+for (let image of images.children) {
+    image.addEventListener('click', () => {
+        resetClasses(images.children, image.id)
+
+        if (image.classList.contains('active')) {
+            image.classList.remove('active');
+        } else {
+            image.classList.add('active');
+        };
+    });
+};
+
+function resetClasses(array, id) {
+    for (let item of array) {
+        if (item.id !== id) {
+            item.classList.remove('active');
+        };
+    };
+}
